e8023e3e8f2018f7ebfed1813bed3d317dcafbe0
amaisuna <taro30143014@gmail.com>
2019-08-15 16:29:19 +0900

    css-loader

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import _ from 'lodash'
-import Tiger from './utilities'
-
-console.log(Tiger.say())
+import './style.css'
 
 function component() {
     const element = document.createElement('div');
-    const array = ["Hello", "webpack", "!!"]
-    element.innerHTML = _.join(array, " ")
-    return element
+    const array = ["Hello", "webpack", "!!"];
+    element.innerHTML = _.join(array, " ");
+    return element;
 }
 
-document.body.appendChild(component())
+document.body.appendChild(component());
+document.body.classList.add('haikei');

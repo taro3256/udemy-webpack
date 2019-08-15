704880beb3c9d4b5346b2a588d1892958bf3b992
amaisuna <taro30143014@gmail.com>
2019-08-16 00:21:56 +0900

    eslint

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,12 @@ module.exports = {
     module: {
         rules: [
             {
+                enforce: "pre",
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader",
+            },
+            {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
